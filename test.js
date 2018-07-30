b0db46273b378847ba88956c9a5c6b9b19a81d2f
e <shenbaoone@gmail.com>
2018-07-31 00:58:48 +0800

    update  02.md 03.md 04.md

--- test.js
+++ test.js
@@ -1,11 +1,50 @@
 
 
+var arr = [1, 2, 3];
+arr.forEach(function (item, index) {
+    console.log(index, item);
+})
+
+var obj = {
+    x: 100,
+    y: 200,
+    z: 300
+}
+var key;
+for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+        console.log(key, obj[key]);
+    }
+}
 
 
+function forEach(obj, fn) {
+    var key;
+    if (obj instanceof Array) {
+        obj.forEach(function (item, index) {
+            fn(item, index);
+        })
+    } else {
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                fn(key, obj[key]);
+            }
+        }
+    }
+}
 
-
-
-
+var arr = [1, 2, 3];
+var obj = {
+    x: 100,
+    y: 200,
+    z: 300
+};
+forEach(arr, function (item, index) {
+    console.log(index, item);
+});
+forEach(obj, function (key, val) {
+    console.log(key, val);
+});
 
 
 

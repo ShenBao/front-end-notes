0609fc0f9a67abb5b5b97300b418d86caa783a52
ShenBao <shenbaoone@gmail.com>
2020-10-02 22:18:21 +0800

    feat: JS 基础知识

--- generate.README.md.js
+++ generate.README.md.js
@@ -7,6 +7,27 @@ var desc = `
 前端学习笔记
 `;
 
+setTimeout( function() {
+  console.log('setTimeout');
+}, 0 )
+
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
+}
+
+async function async2() {
+  console.log('async2');
+}
+
+console.log('script start');
+
+async1();
+
+console.log('script end');
+
+
 var endDesc = `
 ## More links
 
